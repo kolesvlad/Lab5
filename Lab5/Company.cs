b7dc70e34c4879db256c8319f88df9017b85fdc5
valdemar <kolesvlad@gmail.com>
2024-06-18 15:05:20 +0300

    Add: additional attributes

--- Lab5/Company.cs
+++ Lab5/Company.cs
@@ -4,6 +4,7 @@ namespace Lab5;
 
 public class Company
 {
+    
     public string OfficialName { get; set; }
     
     public List<string> Synonyms { get; set; }
@@ -11,6 +12,12 @@ public class Company
     public int MentionCount { get; set; }
 
     public List<string> MentionedTextFileNames { get; set; }
+    
+    public string Headquarters { get; set; }
+    
+    public int EmployeeCount { get; set; }
+    
+    public int OfficeCount { get; set; }
 
     public Company(string officialName, List<string> synonyms)
     {
@@ -24,8 +31,18 @@ public class Company
         return JsonSerializer.Serialize(this);
     }
 
-    public override string ToString()
+    public void SetHeadquarters(string headquarters)
     {
-        return $"{nameof(OfficialName)}: {OfficialName}, {nameof(MentionCount)}: {MentionCount}";
+        Headquarters = headquarters;
+    }
+    
+    public void SetEmployeeCount(int employeeCount)
+    {
+        EmployeeCount = employeeCount;
+    }
+    
+    public void SetOfficeCount(int officeCount)
+    {
+        OfficeCount = officeCount;
     }
 }
