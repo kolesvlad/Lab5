8229b24e9eddeb188d3c455930f5cd76c2025d17
valdemar <kolesvlad@gmail.com>
2024-06-18 15:07:31 +0300

    Add: comments

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -4,14 +4,21 @@ class Program
 {
     static void Main()
     {
+        // Створення екземплярів класів
         var fileManager = new FileManager();
         var textAnalyzer = new TextAnalyzer();
         var reportGenerator = new ReportGenerator();
         
+        // Читаємо файли з диска та записуємо в string
         var originalText = fileManager.ReadTextFiles();
+        
+        // Передпроцесинг тексту
         var preprocessedText = textAnalyzer.PreprocessText(originalText);
+        
+        // Витягування даних про компанії
         var companies = textAnalyzer.ExtractCompaniesInfo(preprocessedText);
         
+        // Створення звіту
         reportGenerator.CreateReport(companies, fileManager);
     }
 }
